2512606baa79a3798a4ccc526df05e6082abaf79
tiroffp@gmail.com <tiroff.p@husky.neu.edu>
2016-10-19 01:14:09 -0400

    basic categorical bar chart

--- HW3/script.js
+++ HW3/script.js
@@ -1,15 +1,22 @@
-var doCatSvg = d3.select("svg"),
-        margin = {top: 50, right: 40, bottom: 40, left: 40},
+var doCatSvg = d3.selectAll(".do-categorical"),
+        margin = {top: 50, right: 80, bottom: 40, left: 40},
         width = +doCatSvg.attr('width') - margin.left - margin.right,
         height = +doCatSvg.attr('height') - margin.top - margin.bottom;
 
 var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
         y = d3.scaleLinear().rangeRound([height, 0]);
+        color = d3.scaleOrdinal(d3.schemeCategory10).domain([' 3 years', ' 2 years', ' 1 year', ' < 1 year']);//.range(['#000', '#888','#0FF','#FFF']);
 
 var g = doCatSvg.append('g')
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+//Variables for legend sizing/spacing
+var legendRectSize = 18;
+var legendSpaceing = 4;
+
 function parse(row) {
+    //change TOTAL column to be total attended, not total missed
+    row['TOTAL'] = 11 - row['TOTAL']
     return row;
 }
 
@@ -17,6 +24,7 @@ function dataLoaded(data) {
 
     //set x domain to be total number of students in the array
     x.domain(data.map(function(d) {return d['Student ID number'];}));
+    y.domain([0,11])
 
     g.append("text")
         .attr('x', 0)
@@ -54,6 +62,41 @@ function dataLoaded(data) {
         .attr('y', -30)
         .attr('text-anchor', 'middle')
         .text('Events Attended');
-}
 
+// data bar
+g.selectAll('.bar')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('x', function(d) { return x(d['Student ID number']); })
+        .attr('y', function(d) {return y(d['TOTAL']); })
+        .attr('width', x.bandwidth())
+        .attr('height', function(d) {return height - y(d['TOTAL'])})
+        .style('fill', function(d) { return color(d['Years in Club'])})
+        .style('stroke', 'black');
+
+    // legend
+    g.selectAll('.legend')
+        .data(color.domain())
+        .enter()
+        .append('g')
+        .attr('class', 'legend')
+        .append('rect')
+        .attr('x',width)
+        .attr('y','0')
+        .attr('width', legendRectSize)
+        .attr('height', legendRectSize)
+        .style('fill', color)
+        .attr('transform', function(d,i) { return 'translate(0,' + (i * (legendRectSize + legendSpaceing)) + ')'})
+        .style('stroke', 'black');
+
+    // legend text
+    g.selectAll('.legend')
+        .append('text')
+        .attr('x',width)
+        .attr('y','0')
+        .attr('transform', function(d,i) { return 'translate('+ (2 + legendRectSize )+',' + (i * (legendRectSize + legendSpaceing) + 15) + ')'})
+        .text(function(d){return d});
+}
 d3.csv('Attendance.csv', parse, dataLoaded);
