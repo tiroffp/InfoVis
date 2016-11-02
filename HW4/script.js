54ff74ab9c3e4eb157bc0fd84ab05e7ad6e54031
tiroffp@gmail.com <tiroff.p@husky.neu.edu>
2016-11-02 00:49:03 -0400

    first commit of HW4, have overall timeline set up with brushing for zoom and tooltip details on demand

---
+++ HW4/script.js
@@ -0,0 +1,50 @@
+    // Define domElement and sourceFile
+    var domElement = "#timeline";
+    var sourceFile = "GOTDeaths.csv";
+
+    function parseData(row){
+        row['start'] = calcChapterNumber(row['Book of Introduction Number'],row['Intro Chapter']);
+        row['end'] = calcChapterNumber(row['Book of Death Number'],row['Death Chapter']);
+        row['lifespan'] = row['end'] - row['start']
+        row['label'] = row['Name'];
+        return row;
+    }
+
+    function calcChapterNumber(book, chapter) {
+        book = parseInt(book)
+        chapter = parseInt(chapter)
+        switch (book) {
+          case 2:
+          chapter = chapter  + 73;
+          break;
+          case 3:
+          chapter = chapter + 143;
+          break;
+          case 4:
+          chapter = chapter + 225;
+          break;
+          case 5:
+          chapter = chapter + 270;
+          break;
+          default:
+          chapter = chapter;
+      }
+      return chapter.toString();
+  }
+
+    // Read in the data and construct the timeline
+    d3.csv(sourceFile, parseData, function(dataset) {
+
+        timeline(domElement)
+        .data(dataset)
+        .band("mainBand", 0.82)
+        .band("naviBand", 0.08)
+        .xAxis("mainBand")
+        .tooltips("mainBand")
+        .xAxis("naviBand")
+        .labels("mainBand")
+        .labels("naviBand")
+        .brush("naviBand", ["mainBand"])
+        .redraw();
+
+    });
