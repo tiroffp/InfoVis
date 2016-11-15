5034c885296f3015201fca3777ed72d095391c47
tiroffp@gmail.com <tiroff.p@husky.neu.edu>
2016-11-14 22:47:47 -0500

    added hw 5, graph with edges, left & right create next move

---
+++ HW5/script.js
@@ -0,0 +1,211 @@
+//sourced from http://bl.ocks.org/d3noob/5155181
+
+// get the data
+d3.csv("game_log.csv",parseData,loadData)
+
+function parseData(row) {
+  row['source'] = row['start_node']
+  row['target'] = row['end_node']
+  row['value'] = row['edge_index']
+  return row
+}
+
+function loadData(error, data) {
+
+ var currStep = 0;
+ var maxStep = 0;
+ var nodes = {};
+ var links = []
+ var edges = []
+
+// Compute the distinct nodes from the links.
+data.forEach(function(d) {
+  if (d['id'] == "") {
+    edges.push(d)
+  }  else {
+    maxStep = Math.max(d['id'],maxStep)
+    d.source = nodes[d.source] ||
+    (nodes[d.source] = {name: d.source});
+    d.target = nodes[d.target] ||
+    (nodes[d.target] = {name: d.target});
+    d.value = +d.value;
+    links.push(d)
+  }});
+
+var width = 960,
+height = 500;
+
+var force = d3.layout.force()
+.nodes(d3.values(nodes))
+.links(links)
+.size([width, height])
+.linkDistance(150)
+.charge(-300)
+.on("tick", tick)
+.start();
+
+// Set the range
+var  v = d3.scale.linear().range([0, 100]);
+
+// Scale the range of the data
+v.domain([0, d3.max(links, function(d) { return d.value; })]);
+
+// asign a type per value to encode opacity
+links.forEach(function(link) {
+  if (v(link.value) <= 25) {
+    link.type = "twofive";
+  } else if (v(link.value) <= 50 && v(link.value) > 25) {
+    link.type = "fivezero";
+  } else if (v(link.value) <= 75 && v(link.value) > 50) {
+    link.type = "sevenfive";
+  } else if (v(link.value) <= 100 && v(link.value) > 75) {
+    link.type = "onezerozero";
+  }
+});
+
+d3.select('body').on("keydown", keydown)
+
+var svg = d3.select("div#chartId")
+.append("div")
+   .classed("svg-container", true) //container class to make it responsive
+   .append("svg")
+   //responsive SVG needs these 2 attributes and no width and height attr
+   .attr("preserveAspectRatio", "xMinYMin meet")
+   .attr("viewBox", "0 0 " + width + " "  + height)
+   //class to make it responsive
+   .classed("svg-content-responsive", true);
+
+// build the arrow.
+svg.append("svg:defs").selectAll("marker")
+    .data(["end"])      // Different link/path types can be defined here
+  .enter().append("svg:marker")    // This section adds in the arrows
+  .attr("id", String)
+  .attr("viewBox", "0 -5 10 10")
+  .attr("refX", 15)
+  .attr("refY", -1.5)
+  .attr("markerWidth", 6)
+  .attr("markerHeight", 6)
+  .attr("orient", "auto")
+  .append("svg:path")
+  .attr("d", "M0,-5L10,0L0,5");
+
+// add the links and the arrows
+var path = svg.append("svg:g").selectAll("path")
+.data(force.links())
+.enter().append("svg:path")
+.attr("class", "link")
+.attr("marker-end", "url(#end)");
+
+var edge = svg.append("svg:g").selectAll("edge")
+.data(force.links())
+.enter().append("svg:path")
+.attr("class", "edge");
+
+// define the nodes
+var node = svg.selectAll(".node")
+.data(force.nodes())
+.enter().append("g")
+.attr("class", "node")
+.on("click", click)
+.on("dblclick", dblclick)
+.call(force.drag);
+
+// add the nodes
+node.append("circle")
+.attr("r", 5);
+
+// add the text
+node.append("text")
+.attr("x", 12)
+.attr("dy", ".35em")
+.text(function(d) { return d.name; });
+
+// add the curvy lines
+function tick() {
+  path.attr("d", function(d) {
+    if (currStep >= d['id'] && d['action_type'] == 'move') {
+      var dx = d.target.x - d.source.x,
+      dy = d.target.y - d.source.y,
+      dr = Math.sqrt(dx * dx + dy * dy);
+      return "M" +
+      d.source.x + "," +
+      d.source.y + "A" +
+      dr + "," + dr + " 0 0,1 " +
+      d.target.x + "," +
+      d.target.y;
+    } else {
+      return "";
+    }
+  })
+  .attr("style", function(d){
+    if(currStep >= d['id'] && d['action_type'] == 'move') {
+      return "opacity: " + Math.round((d['id']/currStep) * 100) + ";"
+    }
+  });
+
+  edge.attr("d", function(d) {
+    var dx = d.target.x - d.source.x,
+    dy = d.target.y - d.source.y,
+    dr = 0;
+    return "M" +
+    d.source.x + "," +
+    d.source.y + "A" +
+    dr + "," + dr + " 0 0,1 " +
+    d.target.x + "," +
+    d.target.y;
+  })
+  .attr("style", function(d) {
+    if(currStep >= d['id'] && d['action_type'] == 'move') {
+      return "opacity: " + Math.round((d['id']/currStep) * 100) + ";"
+    } else if(currStep >= d['id'] && d['action_type'] == 'markedge') {
+      return "stroke-dasharray: 5,0; color: #333;"
+    } else if(currStep >= d['id'] && d['action_type'] == 'unmarkedge') {
+      return "stroke-dasharray: 5,5; color: #AAA;"
+  }});
+
+  node
+  .attr("transform", function(d) {
+    return "translate(" + d.x + "," + d.y + ")"; });
+}
+
+// follow player moves with data with arrow keys
+function keydown() {
+  console.log(d3.event.keyCode, currStep)
+  if (d3.event.keyCode == 37) {
+    currStep = Math.max(0, currStep - 1)
+  } else if (d3.event.keyCode == 39) {
+    currStep = Math.min(currStep + 1, maxStep)
+  }
+}
+
+// action to take on mouse click
+function click() {
+  d3.select(this).select("text").transition()
+  .duration(750)
+  .attr("x", 22)
+  .style("fill", "steelblue")
+  .style("stroke", "lightsteelblue")
+  .style("stroke-width", ".5px")
+  .style("font", "20px sans-serif");
+  d3.select(this).select("circle").transition()
+  .duration(750)
+  .attr("r", 16)
+  .style("fill", "lightsteelblue");
+}
+
+// action to take on mouse double click
+function dblclick() {
+  d3.select(this).select("circle").transition()
+  .duration(750)
+  .attr("r", 6)
+  .style("fill", "#ccc");
+  d3.select(this).select("text").transition()
+  .duration(750)
+  .attr("x", 12)
+  .style("stroke", "none")
+  .style("fill", "black")
+  .style("stroke", "none")
+  .style("font", "10px sans-serif");
+}
+
+};
